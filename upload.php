7cf437653e5775c5764f607f5d8e5eaf4e7f2bf8
Dragos-MIHAI <dragos200019@yahoo.com>
2019-05-10 20:18:06 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -11,12 +11,21 @@ if (isset($_POST['submit])){
 
 
     $fileEXT = explode('.', fileName);
-    ]fileActualExt = strtolower(end($fileExt)); 
+    $fileActualExt = strtolower(end($fileExt)); 
 
     $allowed = array('jpg', 'jpeg', 'png', 'pdf', 'csv');
     
     if (in_array($fileActualExt, $allowed)) {
             if ($fileError ===0) {
+                if($fileSize < 1000000) {
+                        $fileNameNew = uniqid('',true).".".$fileActualExt;
+                        $fileDestination = 'uploads/.$fileNameNew;
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        header("Location: index.php?uploadsucess");
+                }else {
+                    echo "Your file is too big!"
+                }
+
 
             }else {
                 echo "There was an error uploading your file!"; 
